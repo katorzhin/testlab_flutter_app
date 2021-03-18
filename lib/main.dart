12750bd9a635d0bdab376bc87c44d7ee36324980
katorzhin <alex.katorzhin@gmail.com>
2021-03-19 00:47:11 +0200

    added random color and align text to center

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() {
+  runApp(MaterialApp(home: MyPage()));
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => new _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  Random random = new Random();
+
+  Color color;
+
+  void changeRandomColor() {
+    setState(() {
+      color = Color.fromARGB(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+      );
+    });
+  }
+
+  void alignTextToCenter() {
+    Align(
+        alignment: Alignment.center,
+        child:Text(
+        'Привет Flutter!',
+        textDirection: TextDirection.ltr,
+    )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: InkWell(
+          onTap: changeRandomColor,
+          child: Container(
+            color: color,
+          ),
+        ));
+  }
+}
